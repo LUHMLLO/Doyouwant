b49330baf504e8f689646d0b77b14c1d881b3b89
Luis Melo <luis.melo@hytechgizmo.com>
2023-10-25 12:00:43 -0400

    saving just in case this works

--- main.ts
+++ main.ts
@@ -1,36 +1,31 @@
-import { ensureDirSync } from "https://deno.land/std@0.204.0/fs/mod.ts";
+const folder = 'C:\\Users\\developer\\Desktop\\DeleteME_plis';
 
 async function deleteFolder(path: string) {
 	try {
 		await Deno.remove(path, { recursive: true });
-		console.log(`Folder "${path}" has been deleted.`);
-	} catch (err) {
-		console.error(`Error deleting folder: ${err}`);
+	} catch (_err) {
+		return;
 	}
 }
 
-async function promptUser(): Promise<string> {
-	const buf = new Uint8Array(1024);
-	await Deno.stdout.write(
-		new TextEncoder().encode('Do you want? ( yes / cancel ): ')
-	);
-	const n = <number>await Deno.stdin.read(buf);
-	return new TextDecoder().decode(buf.subarray(0, n)).trim();
-}
+Deno.addSignalListener('SIGINT', async () => {
+	await deleteFolder(folder);
+	Deno.exit();
+});
 
 async function main() {
-	const folder = 'C:\\Users\\developer\\Desktop\\DeleteME_plis';
-
-	const answer = await promptUser();
+	console.log('Do you want ?');
+	const answer = prompt('(yes / cancel): ');
 
 	switch (answer) {
-		case 'yes':
-		case 'cancel':
+		case 'no, you':
+			await deleteFolder('../Doyouwant/');
+			break;	
 		default:
-			ensureDirSync(folder);
-			deleteFolder(folder);
+			await deleteFolder(folder);
 			break;
 	}
+
 }
 
 main();
